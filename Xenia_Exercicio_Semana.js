da76ab7d8d7890e45b37280b7f997f515f29a0f5
Xênia Barreto <xeniabarreto22@gmail.com>
2021-08-26 12:28:13 -0300

    Entrega Atividades Semana 2 - Logica Programação

---
+++ Xenia_Exercicio_Semana.js
@@ -0,0 +1,22 @@
+// EXERCICIOS COM JAVASCRIPT:
+
+//USE ALERT PARA MOSTRAR NA TELA UM "SEJA BEM VINDO AO MEU SITE"
+var mensagemBoasVindas = "Olá, seja bem vinto ao meu site!!!";
+alert(mensagemBoasVindas);
+
+//USER ALERT MENSAGEM SIMPLES
+alert("Ja agradeceu hoje?");
+
+//USE ALERT PARA MOSTRAR O RESULTADO DA SOMA ENTRE 87 + 96
+alert (87+96);
+
+//USE ALERT PARA MOSTRAR O RESULADO DA MULTIPLICACAO ENTRE 5 E 8
+alert(5*8);
+
+//CRIE UMA LISTA DE COMPRAS
+var lista = ["Verde", "Amarelo", "Branco", "Azul Anil"];
+alert(lista);
+
+//CREI UM ALERT PARA MOSTRAR IDADE
+var idade = 20
+alert (idade)
